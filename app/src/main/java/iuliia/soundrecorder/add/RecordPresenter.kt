c644fbe59dda86d91a23a619c08bb89ed53d21af
Iuliia <iuliiaponomareva@gmail.com>
2019-11-19 21:10:37 +0300

    Fix file name

--- app/src/main/java/iuliia/soundrecorder/add/RecordPresenter.kt
+++ app/src/main/java/iuliia/soundrecorder/add/RecordPresenter.kt
@@ -15,7 +15,7 @@ class RecordPresenter : RecordContract.Presenter {
 
     override fun onStartRecording() {
         val now = Date()
-        val formattedDate = SimpleDateFormat.getDateTimeInstance().format(now)
+        val formattedDate = SimpleDateFormat("yyyy.MM.dd_HH.mm.ss", Locale.ENGLISH).format(now)
         val fileName = "${view?.getDirectory()}/${formattedDate}.3gp"
         view?.updateUi(true)
         model.startRecording(fileName)
